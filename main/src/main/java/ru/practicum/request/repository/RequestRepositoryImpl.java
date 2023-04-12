da2abb3311b88262a4c687cf1be17a0e412e32b7
Andreikur <andreikur1@yande[.ru>
2023-04-12 17:22:51 +0300

    Save2.18 main в postman без ошибок

--- main/src/main/java/ru/practicum/request/repository/RequestRepositoryImpl.java
+++ main/src/main/java/ru/practicum/request/repository/RequestRepositoryImpl.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Lazy;
 
 public class RequestRepositoryImpl {
     public final RequestRepository requestRepository;
+
     public RequestRepositoryImpl(@Lazy RequestRepository requestRepository) {
         this.requestRepository = requestRepository;
     }
