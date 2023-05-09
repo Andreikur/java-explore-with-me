2a1420fabad621c28e887da5b72ce79c1117f4a1
Andreikur <andreikur1@yande[.ru>
2023-05-09 16:23:33 +0300

    Save 6

--- main/src/main/java/ru/practicum/event/service/EventServiceImpl.java
+++ main/src/main/java/ru/practicum/event/service/EventServiceImpl.java
@@ -26,7 +26,6 @@ import ru.practicum.location.model.Location;
 import ru.practicum.location.repository.LocationRepository;
 import ru.practicum.user.model.User;
 import ru.practicum.user.repository.UserRepository;
-import ru.practicum.viewStats.ViewStatsDto;
 
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaBuilder;
